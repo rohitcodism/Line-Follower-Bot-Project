37de7cc822ff6e4276e0b5ee0857c858613e9ec6
rohitcodism <rohitpaula18i@gmail.com>
2023-06-15 19:07:52 +0530

    do it

---
+++ .spyder-py3/history.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Dec  5 23:29:52 2022)---
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+
+## ---(Sat Dec 17 01:50:30 2022)---
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
